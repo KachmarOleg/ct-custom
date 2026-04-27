1dc131a98a7d51cd0f3cd850f75f5395e0bd741d
oleh.kachmar <oleh.kachmar@castfromclay.co.uk>
2026-04-27 18:35:27 +0200

    Enhance styling for headings, breadcrumbs, and content layout; update header structure to use <main> tag

--- homepage.php
+++ homepage.php
@@ -5,11 +5,23 @@ Template Name: Home page
 get_header();
 ?>
 
+<div class="the-content">
+    <div class="container">
+        <!-- Usually, for breadcrumbs I use the yoast_breadcrumb() function from the Yoast SEO plugin. However, in this case the page logic is a bit unusual. I’m designing a homepage that looks like a “Contact Us” page, while still having “Home” as the root in the breadcrumbs. So I decided just to create styles for it. -->
+        <nav class="breadcrumbs" aria-label="Breadcrumbs">
+            <a href="http://localhost:10064/home/">Home</a>
+            <span>/</span>
+            <a href="http://localhost:10064/?page_id=31">Who we are</a>
+            <span>/</span>
+            <strong>Contact us</strong>
+        </nav>
+
+        <?php the_content(); ?>
+    </div>
+</div>
 
 
-<h1>Contact</h1>
-<h2>Contact</h2>
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam posuere ipsum nec velit mattis elementum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Maecenas eu placerat metus, eget placerat libero. </p>
+
 
 <?php
 $phone   = ct_custom_get_option( 'phone' );
